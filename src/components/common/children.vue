3c946d9eab995980dd2d4b363b30d8f4e675514a
Shangri-la <467527309@qq.com>
2017-06-13 20:53:08 +0800

    父页面和子页面之间的传参问题

---
+++ src/components/common/children.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>我是子组件children</h1>
+        <div>接收到父页面的消息:{{parentMsg}}</div>
+        <el-button type="primary" @click="clickChildren">点击把子组件值传父页面</el-button>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {
+                childrenMsg: '我是children发来的',
+                parentMsg: ''
+            }
+        },
+        props: ['messageFromFather'],
+        methods: {
+            clickChildren(){
+                this.parentMsg = this.messageFromFather
+                this.$emit('child-tell-parent', this.childrenMsg)
+            }
+        },
+        mounted: function () {
+
+
+
+        }
+    }
+</script>
+<style>
+</style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,23 +1,40 @@
 <template>
     <div id="home">
         <h1>首页</h1>
-        <el-button  type="primary" @click="goNavgation">主要按钮</el-button>
+        <el-button type="primary" @click="goNavgation">主要按钮</el-button>
+        <el-button type="primary" @click="pushToChild">向子页面传值</el-button>
+        <p>子组件的信息：{{childmsg}}</p>
+        <!--向子传值-->
+        <children :messageFromFather="fateherMsg" v-on:child-tell-parent="listenchildSomething"></children>
+
     </div>
 
 </template>
 <script>
+    import Children from './common/children.vue'
     export default{
         data(){
-            return{
-                
+            return {
+                fateherMsg: '',
+                childmsg: ''
             }
         },
-        methods:{
+        components: {Children},
+
+        methods: {
             goNavgation(){
                 this.$router.push('nav/slide1')
+            },
+            // 向子类传值
+            pushToChild(){
+                this.fateherMsg = '我的父页面的值'
+            },
+            // 子类的传过来的值
+            listenchildSomething(msg){
+                this.childmsg = msg
             }
         },
-        mounted:function () {
+        mounted: function () {
             console.log($('#home').width());
         }
     }
