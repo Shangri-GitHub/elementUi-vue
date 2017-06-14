7aede657893fd93f7bfab31944a5e4acecd3f3dc
Shangri-la <467527309@qq.com>
2017-06-14 11:48:42 +0800

    添加单选按钮的样式

--- src/components/slide/slide1.vue
+++ src/components/slide/slide1.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1>
-        slide1
-    </h1>
+    <div>
+        <h1>slide1</h1>
+        
+        <el-button-group>
+            <el-button v-for="(item, index) in items" :class="{'el-button--primary':selected==index}" @click="clickBtn(index)">
+                {{item}}
+            </el-button>
+        </el-button-group>
+
+    </div>
+
 </template>
 <script>
+    export default{
+        data(){
+            return {
+                selected: '',
+                items: ['主要按钮', '文字按钮']
+            }
+        },
+        methods: {
+            clickBtn(index){
+                this.selected = index
+            }
+        }
+    }
 </script>
 <style>
+
 </style>
